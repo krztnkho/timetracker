28ec39205124aa65bf17087eab9032559781935b
Rohit Daryanani <rohitdaryanani@yahoo.com>
2016-04-10 14:44:13 +0800

    initial commit

---
+++ app/controller/timetrack.ctrl.js
@@ -0,0 +1,9 @@
+( function TimeTrackCtrl() {
+    var vm = this;
+
+    vm.title = "test";
+
+    angular
+        .module('timetracker')
+        .controller('TimeTrackCtrl', TimeTrackCtrl);
+})();
